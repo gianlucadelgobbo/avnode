1783433e23197087f6314b4741cf15130f242ada
donostiastudio <donostiasocialclub@gmail.com>
2015-11-20 17:35:29 +0100

    Worked on performer jade and css files

--- app/server/routes/user.js
+++ app/server/routes/user.js
@@ -61,7 +61,7 @@ exports.get = function get(req, res) {
 									} else if (output=="xml") {
 										res.render('performer_dett_'+pathArray[1]+"_xml", {	layout: false, userpage:true, title: result.display_name+": "+config.sections[pathArray[1]].title, sez:pathArray[1], result : result, dett : dett, Fnc:Fnc, user : uu });
 									} else {
-										res.render('performer_dett_'+pathArray[1], { userpage:true, title: result.display_name+": "+config.sections[pathArray[1]].title, sez:pathArray[1], result : result, dett : dett, Fnc:Fnc, user : uu });
+										res.render('performer_dett_'+pathArray[1], { userpage:true, title: result.display_name+":  "+config.sections[pathArray[1]].title, sez:pathArray[1], result : result, dett : dett, Fnc:Fnc, user : uu });
 									}
 								} else {
 									res.sendStatus(404);
@@ -71,13 +71,14 @@ exports.get = function get(req, res) {
 							res.sendStatus(404);
 						}
 					} else if (config.sections[pathArray[1]]) {
-						res.render('performer_list', { userpage:true, title: result.display_name+": "+config.sections[pathArray[1]].title, sez:pathArray[1], result : result, Fnc:Fnc, user : uu });
+						res.render('performer_list', { userpage:true, title: result.display_name, title2: config.sections[pathArray[1]].title, sez:pathArray[1], result : result, Fnc:Fnc, user : uu });
+
 					} else {
 						res.sendStatus(404);
 					}
 				} else {
-					console.log("ecchime");
-					console.log(result.files);
+					//- console.log("ecchime");
+					//- console.log(result.files);
 					res.render('performer', { userpage:true, title: result.display_name, result : result, Fnc:Fnc, user : uu });
 				}
 			} else {
