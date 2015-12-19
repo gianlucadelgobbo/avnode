352eae8f768eb5bd692dcb531badffb0164921a3
Stefan Isak <s.isak@zimmer7.com>
2015-12-19 21:33:20 +0100

    Add basic pagination code

--- app/server/helper/index.js
+++ app/server/helper/index.js
@@ -6,13 +6,32 @@ module.exports = {
   date: date,
   pagination: function(link, skip, limit, total) {
     var pages = [];
-    for(var i = 0; i < Math.floor(total / limit); i++) {
-      if ((i * limit) === skip) {
-        var active = true;
-      } else {
-        var active = false;
+    var total = Math.floor(total / limit);
+    var current = Math.floor(skip / limit);
+
+    // add prev link if not on first page
+    if (current !== 0) {
+      pages.push({index: '<<', link: link + 1, active: false});
+      pages.push({index: '<', link: link + current, active: false});
+    }
+
+    // go five items back and forth
+    // TODO could be improved in the future
+    for (var i = (current - 5); i <= (current + 5); i++) {
+      if (i >= 0 && i <= total) {
+        if (i === current) {
+          active = true;
+        } else {
+          active = false;
+        }
+        pages.push({index: (i + 1), link: link + (i + 1), active: active});
       }
-      pages.push({index: (i + 1), link: link + (i + 1), active: active});
+    }
+
+    // add next link if not on first page
+    if (current !== total) {
+      pages.push({index: '>', link: link + (current + 2), active: false});
+      pages.push({index: '>>', link: link + (total + 1), active: false});
     }
     return pages;
   }
