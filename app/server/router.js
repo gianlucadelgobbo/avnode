c28e512bd74853422039f72f22cf55bd849289b7
Stefan Isak <s.isak@zimmer7.com>
2016-01-21 07:35:14 +0100

    Code style

--- app/server/router.js
+++ app/server/router.js
@@ -1,93 +1,54 @@
-var indexRoutes 					= require('./routes/index');
-var performersRoutes 				= require('./routes/performers');
-var performancesRoutes 				= require('./routes/performances');
-var footageRoutes 					= require('./routes/footage');
-var eventsRoutes 					= require('./routes/events');
-var playlistsRoutes 				= require('./routes/playlists');
-var galleriesRoutes 				= require('./routes/galleries');
-var forumRoutes 					= require('./routes/forum');
-var tvshowsRoutes 					= require('./routes/tvshows');
-var userRoutes 				= require('./routes/user');
-var apiRoutes 						= require('./routes/api');
-var searchRoutes 					= require('./routes/search');
+var indexRoutes = require('./routes/index');
+var performersRoutes = require('./routes/performers');
+var performancesRoutes = require('./routes/performances');
+var footageRoutes = require('./routes/footage');
+var eventsRoutes = require('./routes/events');
+var playlistsRoutes = require('./routes/playlists');
+var galleriesRoutes = require('./routes/galleries');
+var forumRoutes = require('./routes/forum');
+var tvshowsRoutes = require('./routes/tvshows');
+var userRoutes = require('./routes/user');
+var apiRoutes = require('./routes/api');
+var searchRoutes = require('./routes/search');
 
-var cpanelLoginRoutes 				= require('./routes/controlpanel/login');
-var cpanelConfirmRoutes 			= require('./routes/controlpanel/confirm');
-var cpanelLogoutRoutes 				= require('./routes/controlpanel/logout');
-var cpanelSignupRoutes 				= require('./routes/controlpanel/signup');
-var cpanelUserRoutes 				= require('./routes/controlpanel/user');
-var cpanelCrewsRoutes 				= require('./routes/controlpanel/crews');
+var cpanelLoginRoutes = require('./routes/controlpanel/login');
+var cpanelConfirmRoutes = require('./routes/controlpanel/confirm');
+var cpanelLogoutRoutes = require('./routes/controlpanel/logout');
+var cpanelSignupRoutes = require('./routes/controlpanel/signup');
+var cpanelUserRoutes = require('./routes/controlpanel/user');
+var cpanelCrewsRoutes = require('./routes/controlpanel/crews');
 
-var uploadRoutes 					= require('./routes/upload');
-var uploadSuccessRoutes				= require('./routes/upload-success');
-var imageRoutes 					= require('./routes/image');
-var ajax		 					= require('./routes/ajax');
+var uploadRoutes = require('./routes/upload');
+var uploadSuccessRoutes = require('./routes/upload-success');
+var imageRoutes = require('./routes/image');
+var ajax = require('./routes/ajax');
 
-var passport 						= require('passport');
-
-/*
-var lostPasswordRoutes = require('./routes/lost-password');
-var resetPasswordRoutes = require('./routes/reset-password');
-var softwareRoutes = require('./routes/software');
-var projectRoutes = require('./routes/project');
-
-var unsubscribeRoutes = require('./routes/unsubscribe');
-var _fpRoutes = require('./routes/_fp');
-var rssRoutes = require('./routes/rss');
-var facebookRoutes = require('./routes/facebook');
-var facebookLPMindexRoutes = require('./routes/facebookLPMindex');
-var flxer5Routes = require('./routes/flxer5');
-var flxerappRoutes = require('./routes/flxerapp');
-*/
+var passport = require('passport');
 
 module.exports = function(app) {
-    // Index //
 	app.get('/', indexRoutes.get);
 
-	// ajax //
-	//app.get('/ajax*', ajax.get);
-	//app.post('/ajax*', ajax.post);
-
-	// performers //
 	app.get('/performers/(:filter)/(:sorting)/(:page)', performersRoutes.get);
 	app.get('/performers(*)', performersRoutes.get);
-	//app.post('/performers*', performersRoutes.post);
 
-	// performances //
 	app.get('/performances/(:filter)/(:sorting)/(:page)', performancesRoutes.get);
 	app.get('/performances(*)', performancesRoutes.get);
-	//app.post('/performances*', performancesRoutes.post);
 
-	// footage //
 	app.get('/footage/(:filter)/(:sorting)/(:page)', footageRoutes.get);
 	app.get('/footage(*)', footageRoutes.get);
-	//app.post('/footage*', footageRoutes.post);
 
-	// events //
 	app.get('/events/(:filter)/(:sorting)/(:page)', eventsRoutes.get);
 	app.get('/events(*)', eventsRoutes.get);
-	//app.post('/events*', eventsRoutes.post);
 
-	// playlists //
 	app.get('/playlists/(:filter)/(:sorting)/(:page)', playlistsRoutes.get);
 	app.get('/playlists(*)', playlistsRoutes.get);
-	//app.post('/playlists*', playlistsRoutes.post);
 
-  // playlists //
 	app.get('/galleries/(:filter)/(:sorting)/(:page)', galleriesRoutes.get);
 	app.get('/galleries(*)', galleriesRoutes.get);
 
-	// forum //
-	//app.get('/forum ', forumRoutes.get);
-	//app.post('/forum ', forumRoutes.post);
-
-	// tvshows //
-	//app.get('/tvshows*', tvshowsRoutes.get);
 	app.get('/tvshows/(:filter)/(:sorting)/(:page)', tvshowsRoutes.get);
 	app.get('/tvshows(*)', tvshowsRoutes.get);
-	//app.post('/tvshows*', tvshowsRoutes.post);
 
-  // local login //
   app.post(
     '/controlpanel/login',
     passport.authenticate('local', { failureRedirect: '/controlpanel/login'}),
@@ -98,13 +59,11 @@ module.exports = function(app) {
   );
 	app.get('/controlpanel/login', cpanelLoginRoutes.get);
 
-  // facebook login //
   app.get('/controlpanel/login/facebook',
       passport.authenticate('facebook', {
           scope: ['public_profile', 'email']
       })
   );
-  // Facebook will redirect the user to this URL after approval.
   app.get('/controlpanel/login/facebook/callback',
       passport.authenticate('facebook', {
           successRedirect: '/',
@@ -112,11 +71,9 @@ module.exports = function(app) {
       })
   );
 
-  // twitter login //
   app.get('/controlpanel/login/twitter',
       passport.authenticate('twitter')
   );
-  // twitter will redirect the user to this URL after approval.
   app.get('/controlpanel/login/twitter/callback',
       passport.authenticate('twitter', {
           successRedirect: '/',
@@ -124,11 +81,9 @@ module.exports = function(app) {
       })
   );
 
-  // google login //
   app.get('/controlpanel/login/google',
       passport.authenticate('google', { scope: ['email','profile'] })
   );
-  // google will redirect the user to this URL after approval.
   app.get('/controlpanel/login/google/callback',
       passport.authenticate('google', {
           successRedirect: '/',
@@ -136,28 +91,13 @@ module.exports = function(app) {
       })
   );
 
-    // cp Log Out //
 	app.get('/controlpanel/logout', cpanelLogoutRoutes.get);
 
-    // local signup //
 	app.get('/controlpanel/signup/', cpanelSignupRoutes.get);
 	app.post('/controlpanel/signup/', cpanelSignupRoutes.post);
 
-	// cp Confirm //
 	app.get('/confirm', cpanelConfirmRoutes.get);
-	//app.post('/confirm', cpanelConfirmRoutes.post);
 
-  // FIXME
-	app.use('/controlpanel*', function(req, res, next) {
-    next();
-    //if (!req.user) {
-    //  res.redirect('/controlpanel/login/?from='+req.url);
-    //} else {
-    //  next();
-    //}
-  });
-
-  // cp User //
   // FIXME
 	app.use('/controlpanel*', function(req, res, next) {
     next();
@@ -175,22 +115,12 @@ module.exports = function(app) {
   app.get('/controlpanel/crews', cpanelCrewsRoutes.getAll);
   app.post('/controlpanel/crews', cpanelCrewsRoutes.post);
 
-	// search //
 	app.get('/search', searchRoutes.get);
-	//app.post('/search', searchRoutes.post);
-
-	// upload //
-	//app.get('/upload', uploadRoutes.get);
-	app.post('/upload', uploadRoutes.post);
-    //app.delete("/uploads/:uuid", uploadRoutes.onDeleteFile);
 
 	// image //
 	app.get('/image', imageRoutes.get);
-	//app.post('/image', imageRoutes.post);
 
-	// Api //
 	app.get('/api/clients', apiRoutes.getClients);
-	//app.get('/api', apiRoutes.get);
 
   app.get('/(:user)/events/(:event)/participate', userRoutes.participateAtUserEvent);
   app.get('/(:user)/events/(:event)', userRoutes.getUserEvent);
@@ -208,6 +138,4 @@ module.exports = function(app) {
   app.get('/(:user)/crews', userRoutes.getUserCrews);
 
   app.get('/(:user)', userRoutes.getUser);
-	//app.post('*', performerRoutes.post);
-	//app.get('*', function(req, res) { res.send(404); });
 };
