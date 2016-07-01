8fe62da387be99cf92c7ae6a8bb7325f6e0c38ec
Gianluca Del Gobbo <g.delgobbo@flyer.it>
2016-07-01 11:51:31 +0200

    New Schemas

--- app/server/schema/location.js
+++ app/server/schema/location.js
@@ -1,7 +1,10 @@
+// location Schema
 var Schema = require('mongoose').Schema;
 
 module.exports = new Schema({
+  formatted_address: String,
   street: String,
+  streetnumber: String,
   city: String,
   zip: String,
   country: String,
--- app/server/schema/locationvenue.js
+++ app/server/schema/locationvenue.js
@@ -1,8 +1,12 @@
+// locationvenue Schema
 var Schema = require('mongoose').Schema;
 
 module.exports = new Schema({
   name: String,
+  // From here same as location
+  formatted_address: String,
   street: String,
+  streetnumber: String,
   city: String,
   zip: String,
   country: String,
